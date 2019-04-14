8fd203d91113e42e302056bc08a2e81fcc868752
AmjadAlakrami <43879892+AmjadAlakrami@users.noreply.github.com>
2019-04-14 15:57:49 +0200

    +

--- web.py
+++ web.py
@@ -2,4 +2,4 @@ import requests
 
 def get(url):
     r = requests.get(url) # hämtar datan från den angivna urlen
-    return r.json() # returnerar datan som json
+    return r.json() # returnerar som en json objekt
